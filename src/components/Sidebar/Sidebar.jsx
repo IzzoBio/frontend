49d574507a99fa126df4e93d00a315c9d1e9e63b
AndoKami <hei.ando.1007@gmail.com>
2024-08-16 21:06:44 +0300

    feat: adding state to the sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,12 @@
+import React, { useState } from 'react';
+
 const Sidebar = () => {
+    const [activeItem, setActiveItem] = useState('');
+
+    const handleClick = (item) => {
+        setActiveItem(item);
+    };
+
     return (
         <div className="w-[300px] bg-green-700 p-6 border-r border-[white] flex flex-col justify-between">
             <div>
@@ -7,11 +15,36 @@ const Sidebar = () => {
                     <p className="text-center m-3">iZZoBio</p>
                 </div>
                 <ul>
-                    <li className="py-2 text-[white] font-bold cursor-pointer hover:text-[white] h-[50px]">Accueil</li>
-                    <li className="py-2 text-[white] cursor-pointer hover:text-[white] h-[50px]">Profile</li>
-                    <li className="py-2 text-[white] cursor-pointer hover:bg-gray-200 hover:text-white h-[50px]">Enregistrement</li>
-                    <li className="py-2 text-[white] cursor-pointer hover:bg-gray-200 hover:text-white h-[50px]">Calendrier</li>
-                    <li className="py-2 text-[white] cursor-pointer hover:bg-gray-200 hover:text-white h-[50px]">Carte</li>
+                    <li 
+                        className={`py-2 text-[white] font-bold cursor-pointer h-[50px] ${activeItem === 'Accueil' ? 'border-r-2 border-gray-200' : 'hover:border-r-2 border-gray-200'}`}
+                        onClick={() => handleClick('Accueil')}
+                    >
+                        Accueil
+                    </li>
+                    <li 
+                        className={`py-2 text-[white] cursor-pointer h-[50px] ${activeItem === 'Profile' ? 'border-r-2 border-gray-200' : 'hover:border-r-2 border-gray-200'}`}
+                        onClick={() => handleClick('Profile')}
+                    >
+                        Profile
+                    </li>
+                    <li 
+                        className={`py-2 text-[white] cursor-pointer h-[50px] ${activeItem === 'Enregistrement' ? 'bg-gray-200 text-white border-r-2 border-gray-200' : 'hover:bg-gray-200 hover:text-white hover:border-r-2 border-gray-200'}`}
+                        onClick={() => handleClick('Enregistrement')}
+                    >
+                        Enregistrement
+                    </li>
+                    <li 
+                        className={`py-2 text-[white] cursor-pointer h-[50px] ${activeItem === 'Calendrier' ? 'bg-gray-200 text-white border-r-2 border-gray-200' : 'hover:bg-gray-200 hover:text-white hover:border-r-2 border-gray-200'}`}
+                        onClick={() => handleClick('Calendrier')}
+                    >
+                        Calendrier
+                    </li>
+                    <li 
+                        className={`py-2 text-[white] cursor-pointer h-[50px] ${activeItem === 'Carte' ? 'bg-gray-200 text-white border-r-2 border-gray-200' : 'hover:bg-gray-200 hover:text-white hover:border-r-2 border-gray-200'}`}
+                        onClick={() => handleClick('Carte')}
+                    >
+                        Carte
+                    </li>
                 </ul>
             </div>
             <div className="text-center border-t border-[white]">
