a08160dae7140a5d422e07b40ccdc44f26923e30
KiadyRafa <kiadyhenikhaja@gmail.com>
2024-08-16 21:28:20 +0200

    feat: add the image

---
+++ src/Data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+      image: '/instru.jpg', 
+      title: 'Champion des Astuces Pro',
+      description: 'Actualités riches en astuces quotidiennes pour le bien-être de l’environnement et de la vie, telles que des remèdes naturels et des conseils adaptés aux variations climatiques'
+    },
+    {
+      image: '/calnedar.jpg', 
+      title: 'Planificateur des Ramassages Efficaces',
+      description: 'Un outil intelligent qui optimise la collecte des déchets en fonction des besoins de la communauté et des contraintes environnementales. Grâce à des algorithmes sophistiqués, ce planificateur identifie les itinéraires les plus efficaces pour les camions de ramassage, réduisant ainsi les émissions de carbone et améliorant la propreté de nos rues'
+    },
+    {
+      image: '/appel.jpg', 
+      title: 'Ramassage sur Demande',
+      description: 'Un service flexible qui permet aux citoyens de demander la collecte de déchets à la demande. Que ce soit pour des encombrants, des matériaux recyclables ou des déchets verts, ce système permet une planification personnalisée des ramassages, réduisant ainsi le gaspillage et favorisant une gestion plus efficace des ressources'
+    },
+    {
+      image: '/poubelle.jpg', 
+      title: 'Renouvellement des Conteneurs de Déchets',
+      description: 'Un programme visant à moderniser et à améliorer les conteneurs de déchets dans nos quartiers. Grâce à des matériaux durables et à des designs ergonomiques, ces nouveaux conteneurs facilitent le tri sélectif et encouragent la participation active des citoyens à la gestion des déchets. Ensemble, nous construisons un avenir plus propre et plus respectueux de l’environnement !'
+    },
+    {
+      image: '/money.jpg', 
+      title: 'Avantages à Obtenir en Collectif',
+      description: 'En collaborant pour la protection de l’environnement, nous récoltons des avantages collectifs. Des économies d’échelle dans la gestion des déchets, une sensibilisation accrue et une plus grande efficacité dans la mise en œuvre de solutions durables sont autant d’avantages que nous obtenons ensemble. Ensemble, nous sommes plus forts pour préserver notre planète '
+    },
+    
+  ];
+  
